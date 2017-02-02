0af94f89ac37f688684373217b06667249ddb9ab
Tommy Jameson <thomas.gregory.jameson@gmail.com>
2017-02-02 13:56:01 -0800

    almost finished with the artists object api

--- src/components/ArtistInfo.js
+++ src/components/ArtistInfo.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 // const key = '6482c833c701e11ea0e0ad3af29e89a1'
-const artist = 'downpresser'
+const artist = 'metallica'
 const spotifyIdUrl = `https://api.spotify.com/v1/search?q=${artist}&type=artist`
 
 class ArtistInfo extends Component{
@@ -11,53 +11,83 @@ class ArtistInfo extends Component{
 
     this.state = {
       artistId: '',
-      genre: '',
-      images: '',
-      topTrack: '',
-      songUrl: '',
-      songName: '',
-      artistName: ''
+      artistName: '',
+      genres: [],
+      images: [],
+      tracks: [],
+      songUrls: []
     }
 
     this.getArtistInformation.call(this, spotifyIdUrl)
   }
 
   getArtistInformation(spotifyIdUrl){
-    const requestForArtistId = axios.get(spotifyIdUrl);
+    const requestForArtistId = axios.get(spotifyIdUrl)
     requestForArtistId.then( data => {
-      console.log(data);
+      // console.log('line 27', data)
        this.setState({
-         artistId: data.data.artists.items[0]
-        //  genre: ////
+         artistId: data.data.artists.items[0].id,
+         genres: data.data.artists.items[0].genres
        })
-       const spotifyDataUrl = `https://api.spotify.com/v1/artists/${this.state.artistId}/top-tracks?country=us`;
+       const spotifyDataUrl = `https://api.spotify.com/v1/artists/${this.state.artistId}/top-tracks?country=us`
        return axios.get(spotifyDataUrl)
-     }).then( data => {
-       console.log(data);
-         this.setState({
-           songUrl: '',
-           songName: '',
-           artistName: ''
-         })
-    })
-}
+    }).then( data => {
 
-  // getArtistInfo(artistId){
-  //   const spotifyDataUrl = `https://api.spotify.com/v1/artists/${artistId}/top-tracks?counrty=us`
-  //   const request = axios.get(spotifyDataUrl)
-  //   request.then((data) => {
-  //      this.setState({
-  //        songUrl: '',
-  //        songName: '',
-  //        artistName: ''
-  //      })
-  //   })
-  // }
+      console.log(data);
+
+      let albumNames = data.data.tracks.map( (album) => {
+        return album.album.name
+      })
+      console.log(albumNames);
+      let tracks = data.data.tracks.map( (track) => {
+        return track.name
+      })
+console.log(tracks)
+      let songUrls = data.data.tracks.map( (song) => {
+        return song.preview_url
+      })
+console.log(songUrls)
+      let nestedImagesArray = data.data.tracks.map( (album) => {
+        return album.album.images
+      })
+
+      let imagesArray = nestedImagesArray.map( (image) => {
+        return image
+      })
+
+      this.setState({
+        artistName: data.data.tracks[0].artists[0].name,
+        albums: albumNames,
+        tracks: tracks,
+        songUrls: songUrls,
+        images: imagesArray
+      })
+    })
+  }
 
   render(){
     return(
       <div>
-        hello
+        <div>
+          <div className="form-control col-xs-3">
+          {this.state.artistId}
+          </div>
+          <div className="form-control col-xs-3">
+          {this.state.artistName}
+          </div>
+          <div className="form-control col-xs-3">
+          {this.state.albums}
+          </div>
+          <div className="form-control col-xs-3">
+          {this.state.tracks}
+          </div>
+          <div className="form-control col-xs-3">
+          {this.state.songUrls}
+          </div>
+          <div className="form-control col-xs-3">
+          {this.state.imagesArray}
+          </div>
+        </div>
       </div>
     )
   }
--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 class Visualizer extends Component{
   constructor(props){
     super(props)
+
   }
 
   render(){
+
     return(
       <div>
 
